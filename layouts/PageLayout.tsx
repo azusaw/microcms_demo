5fe71d5b3bd43d0e3e9c85a0aa2202fa48bcbaf3
azusaw <azusa0304@gmail.com>
2021-03-06 00:05:21 +0900

    changed header design

--- layouts/PageLayout.tsx
+++ layouts/PageLayout.tsx
@@ -1,22 +1,32 @@
 import React from "react"
+import Link from "next/link"
 import { motion } from "framer-motion"
 import style from "./page-layout.module.css"
+import { Button } from "antd"
 
 export default function PageLayout({ children }) {
   return (
     <>
-      <div className={style.headerBg} />
-      <h1 className={`${style.headerTitle} `}>
+      <Link href="/">
+        <div className={style.headerBg} />
+      </Link>
+      <div className={style.headerText}>
+        <span
+          style={{ marginBottom: "10px", fontWeight: 400, fontSize: "18px" }}
+        >
+          {"* Click Header and Back to Home"}
+        </span>
+        <br />
         {"Various Programming"}
         <br />
-        {"Languages"}
-      </h1>
-      <div className={style.children}>{children}</div>
+        {"Technologies"}
+      </div>
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ delay: 0.3 }}
       >
+        <div className={style.children}>{children}</div>
       </motion.div>
       <div className={style.footer} />
     </>
